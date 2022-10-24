00eb89d13fbe9f84dda91f824c31378b85e4493d
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-10-24 04:09:19 +0200

    implement oauth2 authorization server

--- src/main/java/com/bsib/authorizationserver/AuthorizationServerApplication.java
+++ src/main/java/com/bsib/authorizationserver/AuthorizationServerApplication.java
@@ -1,13 +1,35 @@
 package com.bsib.authorizationserver;
 
+import com.bsib.authorizationserver.users.User;
+import com.bsib.authorizationserver.users.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class AuthorizationServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthorizationServerApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner dataLoader(UserRepository repo,
+                                 PasswordEncoder encoder) {
+        return args -> {
+            repo.save(
+                    new User("blessed",
+                            encoder.encode("password"),
+                            "ROLE_ADMIN")
+            );
+            repo.save(
+                    new User("chiedza",
+                            encoder.encode("password"),
+                            "ROLE_ADMIN")
+            );
+        };
     }
 
 }
---
+++ src/main/java/com/bsib/authorizationserver/users/UserRepository.java
@@ -0,0 +1,7 @@
+package com.bsib.authorizationserver.users;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsername(String username);
+}
